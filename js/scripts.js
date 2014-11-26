ec5856a4a290d6a2a6b68d9d952cf4b569937146
th3g <th3g@mtmail.mtsu.edu>
2014-11-26 16:51:35 -0600

    Idea

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-
-$(document).ready(function(){$('#sidebar').affix({
-      offset: {
-        top: 230,
-        bottom: 100
-      }
-});	
-$('#midCol').affix({
-      offset: {
-        top: 230,
-        bottom: 100
-      }
-});	
-});
-
 $(document).ready(function(){
 
 $("#signup").validate({
