608a311ca91c9ee38ad205e9368f2571662b41bc
John Griner <johngriner91@gmail.com>
2014-12-01 08:20:13 -0600

    Testing downloading from server

---
+++ downloadFile.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<?php
+$id = mysqli_real_escape_string($link, $_GET['id']);
+$SELECT = "SELECT * FROM FILETESTING WHERE filename = $id";
+$result = mysqli_query($SELECT, $link);
+$result = mysqli_fetch_assoc($result);
+
+header("content-type: $result['filecontent']");
+echo $result['data'];
+?>
+<html lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <title>Project Sign in</title>
+  <meta name="generator" content="Bootply" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <!-- Custom CSS -->
+  <link href="css/landing-page.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+
+</head>
+<body>
+
+  <nav class="navbar navbar-static">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" target="ext"><b>Schneider Electric</b></a>
+        <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="glyphicon glyphicon-chevron-down"></span>
+        </a>
+      </div>
+    </div>
+  </nav><!-- /.navbar -->
+
+  <div class="container">
+
+    <a href="index.html">Back</a>
+  </div>
+
+  <!-- script references -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
