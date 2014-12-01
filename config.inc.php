a9dfb7a8dc6bc113611534b81ed6301495c4c7a0
snyderjams <snyderjams@gmail.com>
2014-12-01 13:00:48 -0600

    updated config file

--- config.inc.php
+++ config.inc.php
@@ -1,21 +1,13 @@
 <?php
-
-	$host = "mysql.cs.mtsu.edu";
+	
+	$server = "mysql.cs.mtsu.edu";
 	$dbname = "jls7h";
 	$username = "jls7h";
 	$password = "database2014";
 
-	//Connect to MSSQL
-	$dbhandle = mysql_connect($host, $username, $password);
-
-	if (!$dbhandle){
-	    die("Couldn't connect to $host");
-	}
-
-	//Select DB
-	$selected = mysql_select_db($dbname);
-
-	if(!$selected){
-		die("Couldn't open database $dbname");
+	$db = new mysqli($server, $username, $password, $dbname);
+	
+	if($db->connect_error){
+		die("CONNECTION FAILED: " . $db->connect_error);
 	}
 ?>
