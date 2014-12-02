031e368ef5cd42776c8e9918b8f1502af0be6b31
John Griner <johngriner91@gmail.com>
2014-12-02 17:07:06 -0600

    Attachments work..... sort of

--- editTag.php
+++ editTag.php
@@ -285,12 +285,7 @@
 							</table> <br>
 							<table align="center">
 								<tr>
-									<td width="246">
-										<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
-										<input name="userfile" type="file" id="userfile">
-									</td>
-									<td width="80"><input name="upload" type="submit" class="box" id="upload" onclick="upload()" value=" Upload "></td>
-								</tr>
+									<th><a href="uploadFile.html"><button type="button" style="width:150px" class="button">Add Attachments</button></a></th>
 							</table>
 
 					  </div><!--/panel-body-->
