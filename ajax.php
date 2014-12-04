37222ba73985ad9ddea94681784d93372a7815dd
snyderjams <snyderjams@gmail.com>
2014-12-04 00:42:13 -0600

    fixed search forreal

--- ajax.php
+++ ajax.php
@@ -578,14 +578,11 @@ function search(){
 	$query = "SELECT * FROM view_TAGS ";
 
 	if(count($conditions) > 0){
-		$query .= "WHERE " . implode(' AND ', $conditions);
-	}
-
-	if($_POST['Obsolete'] == 1 && count($conditions) > 0){
-		$query .= "AND Obsolete LIKE '%" . $_POST['Obsolete'] . "%'";
+		$query .= "WHERE " . implode(' AND ', $conditions) . "AND Obsolete LIKE '%" . $_POST['Obsolete'] . "%'";
 	}else{
 		$query .= "WHERE Obsolete LIKE '%" . $_POST['Obsolete'] . "%'";
 	}
+
 
 	echo $query;
 	
